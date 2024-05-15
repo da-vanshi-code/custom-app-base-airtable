b13078d3b89f6e5b11b3a375726cd20ab07a41f5
Devanshi Udeshi <devanshiudeshi@Devanshis-MacBook-Pro.local>
2024-05-14 21:45:56 -0400

    deleting commented lines

--- src/components/StatusDropDown.tsx
+++ src/components/StatusDropDown.tsx
@@ -3,49 +3,10 @@
 import {Task, Status} from '@/components/TaskView';
 import { getTaskLists, updateTaskStatus } from '@/utils/airtable';
 
-// interface DropdownProps {
-//     initialValue: Status;
-//     onChange: (newStatus: Status) => void;
-// }
 interface DropdownProps {
     initialValue: Status;
     taskId: string;
 }
-
-// export const StatusDropdown: React.FC<DropdownProps> = ({ initialValue, onChange }) => {
-//     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-//         const value = event.target.value;
-//         const enumValue = Status[value as keyof typeof Status]; // Convert string to enum
-//         if (enumValue !== undefined) {
-//             onChange(enumValue);
-//         } else {
-//             console.error("Invalid status value");
-//         }
-//     };
-
-//     return (
-//         <select value={initialValue} onChange={handleChange}>
-//             {Object.values(Status).map(status => (
-//                 <option key={status} value={status}>{status}</option>
-//             ))}
-//         </select>
-//     );
-// };
-
-// export const StatusDropdown: React.FC<DropdownProps> = ({ initialValue, onChange }) => {
-//     return (
-//         <select value={initialValue} onChange={(e) => onChange(e.target.value as Status)}>
-//             {Object.values(Status).map(status => (
-//                 <option key={status} value={status}>{status}</option>
-//             ))}
-//         </select>
-//     );
-// };
-
-// const handleStatusChange = async (taskId: string, newStatus: Status) => {
-//     console.log("I am getting called")
-//     await updateTaskStatus(taskId, newStatus);
-// };
 
 export const StatusDropdown: React.FC<DropdownProps> = ({ initialValue, taskId }) => {
     return (
